b92b0b56fce4e83c61ea3b5035fe82290cc1fd5c
Nazmus Sakib <nazmussakib@nazmuss-mini.home>
2023-06-06 12:23:42 -0400

    initial commit

---
+++ src/ComponentRenderer.js
@@ -0,0 +1,254 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import AnimationRevealPage from "helpers/AnimationRevealPage.js"
+
+import EventLandingPage from "demos/EventLandingPage.js";
+import HotelTravelLandingPage from "demos/HotelTravelLandingPage.js";
+import AgencyLandingPage from "demos/AgencyLandingPage.js";
+import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
+import RestaurantLandingPage from "demos/RestaurantLandingPage.js";
+import ServiceLandingPage from "demos/ServiceLandingPage.js";
+import HostingCloudLandingPage from "demos/HostingCloudLandingPage.js";
+
+import EventLandingPageImageSrc from "images/demo/EventLandingPage.jpeg";
+import HotelTravelLandingPageImageSrc from "images/demo/HotelTravelLandingPage.jpeg";
+import AgencyLandingPageImageSrc from "images/demo/AgencyLandingPage.jpeg";
+import SaaSProductLandingPageImageSrc from "images/demo/SaaSProductLandingPage.jpeg";
+import RestaurantLandingPageImageSrc from "images/demo/RestaurantLandingPage.jpeg";
+import ServiceLandingPageImageSrc from "images/demo/ServiceLandingPage.jpeg";
+import HostingCloudLandingPageImageSrc from "images/demo/HostingCloudLandingPage.jpeg";
+
+
+import LoginPage from "pages/Login.js";
+import SignupPage from "pages/Signup.js";
+import PricingPage from "pages/Pricing.js";
+import AboutUsPage from "pages/AboutUs.js";
+import ContactUsPage from "pages/ContactUs.js";
+import BlogIndexPage from "pages/BlogIndex.js";
+import TermsOfServicePage from "pages/TermsOfService.js";
+import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+
+import LoginPageImageSrc from "images/demo/LoginPage.jpeg";
+import SignupPageImageSrc from "images/demo/SignupPage.jpeg";
+import PricingPageImageSrc from "images/demo/PricingPage.jpeg";
+import AboutUsPageImageSrc from "images/demo/AboutUsPage.jpeg";
+import ContactUsPageImageSrc from "images/demo/ContactUsPage.jpeg";
+import BlogIndexPageImageSrc from "images/demo/BlogIndexPage.jpeg";
+import TermsOfServicePageImageSrc from "images/demo/TermsOfServicePage.jpeg";
+import PrivacyPolicyPageImageSrc from "images/demo/PrivacyPolicyPage.jpeg";
+
+import ThreePlansPricing from "components/pricing/ThreePlans.js";
+
+import ThreeColWithSideImageFeatures from "components/features/ThreeColWithSideImage.js";
+import TwoColWithButtonFeatures from "components/features/TwoColWithButton.js";
+
+import TrendingCards from "components/cards/TwoTrendingPreviewCardsWithImage.js";
+
+import PopularAndRecentPostsBlog from "components/blogs/PopularAndRecentBlogPosts.js";
+
+import TwoColumnWithImageTestimonial from "components/testimonials/TwoColumnWithImage.js";
+import SimpleWithSideImageFAQS from "components/faqs/SimpleWithSideImage.js";
+import TwoColContactUsForm from "components/forms/TwoColContactUsWithIllustration.js";
+import MiniCenteredFooter from "components/footers/MiniCenteredFooter.js";
+
+
+export const components = {
+  landingPages: {
+    RestaurantLandingPage: {
+      component: RestaurantLandingPage,
+      imageSrc: RestaurantLandingPageImageSrc,
+      url: "/components/landingPages/RestaurantLandingPage",
+    },
+    HotelTravelLandingPage: {
+      component: HotelTravelLandingPage,
+      imageSrc: HotelTravelLandingPageImageSrc,
+      url: "/components/landingPages/HotelTravelLandingPage",
+    },
+    SaaSProductLandingPage: {
+      component: SaaSProductLandingPage,
+      imageSrc: SaaSProductLandingPageImageSrc,
+      url: "/components/landingPages/SaaSProductLandingPage",
+    },
+    ServiceLandingPage: {
+      component: ServiceLandingPage,
+      imageSrc: ServiceLandingPageImageSrc,
+      url: "/components/landingPages/ServiceLandingPage",
+    },
+    EventLandingPage: {
+      component: EventLandingPage,
+      imageSrc: EventLandingPageImageSrc,
+      url: "/components/landingPages/EventLandingPage",
+    },
+    AgencyLandingPage: {
+      component: AgencyLandingPage,
+      imageSrc: AgencyLandingPageImageSrc,
+      url: "/components/landingPages/AgencyLandingPage",
+    },
+    HostingCloudLandingPage: {
+      component: HostingCloudLandingPage,
+      imageSrc: HostingCloudLandingPageImageSrc,
+      url: "/components/landingPages/HostingCloudLandingPage",
+    },
+  },
+
+  innerPages: {
+    LoginPage: {
+      component: LoginPage,
+      imageSrc: LoginPageImageSrc,
+      scrollAnimationDisabled: true,
+      url: "/components/innerPages/LoginPage",
+    },
+    SignupPage: {
+      component: SignupPage,
+      url: `/components/innerPages/SignupPage`,
+      imageSrc: SignupPageImageSrc,
+      scrollAnimationDisabled: true,
+    },
+    PricingPage: {
+      component: PricingPage,
+      url: `/components/innerPages/PricingPage`,
+      imageSrc: PricingPageImageSrc,
+    },
+    AboutUsPage: {
+      component: AboutUsPage,
+      url: `/components/innerPages/AboutUsPage`,
+      imageSrc: AboutUsPageImageSrc,
+    },
+    ContactUsPage: {
+      component: ContactUsPage,
+      url: `/components/innerPages/ContactUsPage`,
+      imageSrc: ContactUsPageImageSrc,
+    },
+    BlogIndexPage: {
+      component: BlogIndexPage,
+      url: `/components/innerPages/BlogIndexPage`,
+      imageSrc: BlogIndexPageImageSrc,
+    },
+    TermsOfServicePage: {
+      component: TermsOfServicePage,
+      url: `/components/innerPages/TermsOfServicePage`,
+      imageSrc: TermsOfServicePageImageSrc,
+    },
+    PrivacyPolicyPage: {
+      component: PrivacyPolicyPage,
+      url: `/components/innerPages/PrivacyPolicyPage`,
+      imageSrc: PrivacyPolicyPageImageSrc,
+    }
+  },
+
+  blocks: {    
+    Pricing: {      
+      elements: {     
+        ThreePlans: {
+          name: "Simple Three Plans",
+          component: ThreePlansPricing,
+          url: "/components/blocks/Pricing/ThreePlans",
+        },
+      }
+    },
+    Features: {
+      type: "Features Section",
+      elements: {
+        ThreeColWithSideImage: {
+          name: "Three Column With Side Image",
+          component: ThreeColWithSideImageFeatures,
+          url: "/components/blocks/Features/ThreeColWithSideImage",
+        },
+         TwoColWithButton: {
+          name: "Two Column With Image and Action Button",
+          component: TwoColWithButtonFeatures,
+          url: "/components/blocks/Features/TwoColWithButton",
+        },        
+      }
+    },
+
+    Cards: {
+      type: "Cards",
+      elements: {       
+        Trending: {
+          name: "Two Trending Preview Cards With Images",
+          component: TrendingCards,
+          url: "/components/blocks/Cards/Trending",
+        },
+      }
+    },
+
+    Blog: {
+      type: "Blog Section",
+      elements: {      
+        PopularAndRecentPosts: {
+          name: "Popular And Recent Posts",
+          component: PopularAndRecentPostsBlog,
+          url: "/components/blocks/Blog/PopularAndRecentPosts",
+        },     
+      } 
+    },
+
+    Testimonial: {
+      type: "Testimonial Section",
+      elements: {
+        TwoColumnWithImage: {
+          name: "Two Column With Image",
+          component: TwoColumnWithImageTestimonial,
+          url: "/components/blocks/Testimonial/TwoColumnWithImage",
+        }        
+      }
+    },
+
+    FAQS: {
+      type: "FAQs Section",
+      elements: {
+        SimpleWithSideImage: {
+          name: "Simple With Side Image",
+          component: SimpleWithSideImageFAQS,
+          url: "/components/blocks/FAQS/SimpleWithSideImage",
+        }        
+      }
+    },
+
+    Form: {
+      type: "Forms Section",
+      elements: {     
+        TwoColContactUs: {
+          name: "Two Column Contact Us",
+          component: TwoColContactUsForm,
+          url: "/components/blocks/Form/TwoColContactUs",
+        }       
+      }
+    },
+    Footer: {
+      type: "Footers Section",
+      elements: {     
+        MiniCentered: {         
+          component: MiniCenteredFooter,
+          url: "/components/blocks/Footer/MiniCentered",
+        },
+      }
+    }
+  }
+}
+
+export default () => {
+  const { type, subtype, name } = useParams()
+
+  try {
+    let Component = null;
+    if(type === "blocks" && subtype) {
+      Component= components[type][subtype]["elements"][name].component
+      return <AnimationRevealPage disabled>
+          <Component/>
+        </AnimationRevealPage>
+    }
+    else
+      Component= components[type][name].component
+
+    if(Component)
+      return <Component/>
+
+    throw new Error("Component Not Found")
+  }
+  catch (e) {
+    console.log(e)
+    return <div>Error: Component Not Found</div>
+  }
+}
