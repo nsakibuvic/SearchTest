3c941b1376617baa8d1305e39a8f03232a503d4a
Nazmus Sakib <nazmussakib@nazmuss-mini.home>
2023-06-07 13:44:27 -0400

    eight commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import GlobalStyles from 'styles/GlobalStyles';
 import { css } from "styled-components/macro"; //eslint-disable-line
 
 import ComponentRenderer from "ComponentRenderer.js";
-import MainLandingPage from "MainLandingPage.js";
-import ThankYouPage from "ThankYouPage.js";
+import MainLandingPage from "pages/MainLandingPage.js";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -29,8 +28,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/components/:type/:subtype/:name" element={<ComponentRenderer />} />
-          <Route path="/components/:type/:name" element={<ComponentRenderer />} />
-          <Route path="/thank-you" element={<ThankYouPage />} />
+          <Route path="/components/:type/:name" element={<ComponentRenderer />} />          
           <Route path="/" element={<MainLandingPage />} />
 
           {/* Inner Pages */}
