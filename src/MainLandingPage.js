4505822bc46f68a4c09a49b516e6d98175e5d5f6
Nazmus Sakib <nazmussakib@nazmuss-mini.home>
2023-06-07 13:02:33 -0400

    fifth commit

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -9,8 +9,6 @@ import {
 } from "components/misc/Layouts";
 import tw from "twin.macro";
 import styled from "styled-components";
-import { css } from "styled-components/macro";
-import { LogoLink } from "components/headers/light.js";
 import { SectionHeading as HeadingBase } from "components/misc/Headings";
 import { SectionDescription as DescriptionBase } from "components/misc/Typography";
 
@@ -25,11 +23,6 @@ import MiniCenteredFooter from "components/footers/MiniCenteredFooter";
 
 /* Hero */
 const Row = tw.div`flex`;
-const NavRow = tw(Row)`flex flex-col lg:flex-row items-center justify-between`;
-const NavLink = tw.a`mt-4 lg:mt-0 transition duration-300 font-medium pb-1 border-b-2 mr-12 text-gray-700 border-gray-400 hocus:border-gray-700`;
-const PrimaryNavLink = tw(
-	NavLink
-)`text-gray-100 bg-primary-500 px-6 py-3 border-none rounded hocus:bg-primary-900 focus:shadow-outline mt-6 md:mt-4 lg:mt-0`;
 const HeroRow = tw(
 	Row
 )`flex-col lg:flex-row justify-between items-center pt-8 lg:pt-12 pb-16 max-w-screen-2xl mx-auto flex-wrap`;
